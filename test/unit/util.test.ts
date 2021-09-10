379d14f45eedc46736f64bb5c37a7f906e235d4c
Peter Hale <peter.hale@salesforce.com>
2021-09-10 11:31:01 -0600

    feat: add help section helper function
    @W-9791511@

--- test/unit/util.test.ts
+++ test/unit/util.test.ts
@@ -8,7 +8,10 @@
 import { expect } from 'chai';
 import { Separator } from 'inquirer';
 import stripAnsi = require('strip-ansi');
-import { generateTableChoices } from '../../src/util';
+import { EnvironmentVariable, SUPPORTED_ENV_VARS } from '@salesforce/core';
+import { ORG_CONFIG_ALLOWED_PROPERTIES, OrgConfigProperties } from '@salesforce/core';
+import { SfdxPropertyKeys, SFDX_ALLOWED_PROPERTIES } from '@salesforce/core';
+import { generateTableChoices, toHelpSection } from '../../src/util';
 
 describe('generateTableChoices', () => {
   const columns = {
@@ -46,5 +49,63 @@ describe('generateTableChoices', () => {
     expect(stripAnsi(separator.toString())).to.be.equal('APP OR PACKAGE TYPE       PATH      ');
     expect(tableChoices[1]).to.have.property('name').and.equal('force-app      org        force-app ');
     expect(tableChoices[2]).to.have.property('name').and.equal('my-app         a-long-org my-app    ');
+  });
+});
+
+describe('toHelpSection', () => {
+  it('should produce help section for env vars', () => {
+    const envVarSection = toHelpSection('ENV VAR SECTION', EnvironmentVariable.SFDX_ACCESS_TOKEN);
+    expect(envVarSection).to.have.property('header', 'ENV VAR SECTION');
+    expect(envVarSection).to.have.property('body').to.have.property('length', 1);
+    expect(envVarSection.body[0]).to.deep.equal({
+      name: 'SFDX_ACCESS_TOKEN',
+      description: SUPPORTED_ENV_VARS[EnvironmentVariable.SFDX_ACCESS_TOKEN].description,
+    });
+  });
+  it('should produce help section for org config vars', () => {
+    const orgConfigSection = toHelpSection('ORG CONFIG VAR SECTION', OrgConfigProperties.TARGET_ORG);
+    expect(orgConfigSection).to.have.property('header', 'ORG CONFIG VAR SECTION');
+    expect(orgConfigSection).to.have.property('body').to.have.property('length', 1);
+    const orgConfig = ORG_CONFIG_ALLOWED_PROPERTIES.find(({ key }) => key === OrgConfigProperties.TARGET_ORG);
+    expect(orgConfigSection.body[0]).to.deep.equal({
+      name: 'target-org',
+      description: orgConfig.description,
+    });
+  });
+  it('should produce help section for sfdx config vars', () => {
+    const sfdxConfigSection = toHelpSection('SFDX CONFIG VAR SECTION', SfdxPropertyKeys.INSTANCE_URL);
+    expect(sfdxConfigSection).to.have.property('header', 'SFDX CONFIG VAR SECTION');
+    expect(sfdxConfigSection).to.have.property('body').to.have.property('length', 1);
+    const sfdxConfig = SFDX_ALLOWED_PROPERTIES.find(({ key }) => key === SfdxPropertyKeys.INSTANCE_URL);
+    expect(sfdxConfigSection.body[0]).to.deep.equal({
+      name: 'instanceUrl',
+      description: sfdxConfig.description,
+    });
+  });
+  it('should produce help section for mixed config vars', () => {
+    const mixedSection = toHelpSection(
+      'MIXED VAR SECTION',
+      EnvironmentVariable.SFDX_ACCESS_TOKEN,
+      OrgConfigProperties.TARGET_ORG,
+      SfdxPropertyKeys.INSTANCE_URL
+    );
+    expect(mixedSection).to.have.property('header', 'MIXED VAR SECTION');
+    expect(mixedSection).to.have.property('body').to.have.property('length', 3);
+    const sfdxConfig = SFDX_ALLOWED_PROPERTIES.find(({ key }) => key === SfdxPropertyKeys.INSTANCE_URL);
+    const orgConfig = ORG_CONFIG_ALLOWED_PROPERTIES.find(({ key }) => key === OrgConfigProperties.TARGET_ORG);
+    expect(mixedSection.body).to.deep.equal([
+      {
+        name: 'SFDX_ACCESS_TOKEN',
+        description: SUPPORTED_ENV_VARS[EnvironmentVariable.SFDX_ACCESS_TOKEN].description,
+      },
+      {
+        name: 'target-org',
+        description: orgConfig.description,
+      },
+      {
+        name: 'instanceUrl',
+        description: sfdxConfig.description,
+      },
+    ]);
   });
 });
