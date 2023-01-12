8078c7b828809185bd899f37249b99e411cc70e1
mshanemc <shane.mclaughlin@salesforce.com>
2023-01-12 17:25:39 -0600

    fix: potential zero-plugin configs

--- src/sfCommand.ts
+++ src/sfCommand.ts
@@ -373,7 +373,8 @@ export abstract class SfCommand<T> extends Command {
       commandId: this.id,
     };
     // what hooks are there in the plugins?  Subscribe to matching lifecycle events
-    this.config.plugins
+    // there might not be any plugin in certain UT scenarios when _run is called
+    (this.config.plugins ?? [])
       // omit oclif and telemetry (which subscribes itself to events already)
       .filter((plugin) => !plugin.name.startsWith('@oclif/') && plugin.name !== '@salesforce/plugin-telemetry')
       .flatMap((p) => Object.entries(p.hooks))
