32597ddb3f16971a98b1666ffb7afc1aa118dc67
Mike Donnalley <mdonnalley@salesforce.com>
2024-04-16 10:12:51 -0600

    test: remove test

--- test/integration/ux/table.integration.ts
+++ test/integration/ux/table.integration.ts
@@ -8,64 +8,6 @@ import { expect } from 'chai';
 import stripAnsi from 'strip-ansi';
 import { table } from '../../../src/ux/table.js';
 
-/* eslint-disable camelcase */
-const apps = [
-  {
-    build_stack: {
-      id: '123',
-      name: 'heroku-16',
-    },
-    created_at: '2000-01-01T22:34:46Z',
-    id: '123',
-    git_url: 'https://git.heroku.com/supertable-test-1.git',
-    name: 'supertable-test-1',
-    owner: {
-      email: 'example@heroku.com',
-      id: '1',
-    },
-    region: { id: '123', name: 'us' },
-    released_at: '2000-01-01T22:34:46Z',
-    stack: {
-      id: '123',
-      name: 'heroku-16',
-    },
-    updated_at: '2000-01-01T22:34:46Z',
-    web_url: 'https://supertable-test-1.herokuapp.com/',
-  },
-  {
-    build_stack: {
-      id: '321',
-      name: 'heroku-16',
-    },
-    created_at: '2000-01-01T22:34:46Z',
-    id: '321',
-    git_url: 'https://git.heroku.com/phishing-demo.git',
-    name: 'supertable-test-2',
-    owner: {
-      email: 'example@heroku.com',
-      id: '1',
-    },
-    region: { id: '321', name: 'us' },
-    released_at: '2000-01-01T22:34:46Z',
-    stack: {
-      id: '321',
-      name: 'heroku-16',
-    },
-    updated_at: '2000-01-01T22:34:46Z',
-    web_url: 'https://supertable-test-2.herokuapp.com/',
-  },
-];
-
-const columns = {
-  id: { header: 'ID' },
-  name: {},
-  web_url: { extended: true },
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-  stack: { extended: true, get: (r: any) => r.stack?.name },
-};
-
-const ws = ' ';
-
 describe('table', () => {
   let output = '';
   function printLine(line: string) {
@@ -74,15 +16,6 @@ describe('table', () => {
 
   afterEach(() => {
     output = '';
-  });
-
-  // This can't be in the unit tests since wireit changes process.stdout.isTTY, which alters the behavior of table
-  it('should not truncate in TTY env', () => {
-    const three = { ...apps[0], id: '0'.repeat(80), name: 'supertable-test-3' };
-    table([...apps, three], columns, { filter: 'id=0', printLine, truncate: false });
-    expect(output).to.equal(` ID${ws.padEnd(78)} Name${ws.padEnd(14)}
- ${''.padEnd(three.id.length, '─')} ─────────────────${ws}
- ${three.id} supertable-test-3${ws}\n`);
   });
 
   it('does not exceed stack depth on very tall tables', () => {
