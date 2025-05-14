5d193cf4a7aa59dac2506ea81f3d11dd9f93b6b4
Esteban Romero <30849643+EstebanRomero84@users.noreply.github.com>
2025-05-14 17:14:04 -0300

    fix: pretty-print `MULTIPLE_API_ERROR` data (#677)
    * fix: add logic to handle multiple errors returned from api call
    
    * fix: reduce the scope to only  jsforce error MULTIPLE_API_ERRORS
    
    * chore: typo
    
    * chore: remove TS assertions (#680)
    
    ---------
    
    Co-authored-by: Cristian Dominguez <6853656+cristiand391@users.noreply.github.com>

--- test/unit/errorFormatting.test.ts
+++ test/unit/errorFormatting.test.ts
@@ -79,4 +79,28 @@ describe('errorFormatting.formatError()', () => {
     expect(errorOutput).to.contain('warnings: undefined');
     expect(errorOutput).to.contain('result: undefined');
   });
+
+  it('should have correct output for multiple errors in table format when errorCode is MULTIPLE_API_ERRORS', () => {
+    const innerError = SfError.create({
+      message: 'foo',
+      data: [
+        { errorCode: 'ERROR_1', message: 'error 1' },
+        { errorCode: 'ERROR_2', message: 'error 2' },
+      ],
+    });
+    const sfError = SfError.create({
+      name: 'myError',
+      message: 'foo',
+      actions: ['bar'],
+      context: 'myContext',
+      exitCode: 8,
+      cause: innerError,
+    });
+    const err = SfCommandError.from(sfError, 'thecommand');
+    err.code = 'MULTIPLE_API_ERRORS';
+    const errorOutput = formatError(err);
+    expect(errorOutput).to.match(/Error Code.+Message/);
+    expect(errorOutput).to.match(/ERROR_1.+error 1/);
+    expect(errorOutput).to.match(/ERROR_2.+error 2/);
+  });
 });
